b56c3cb7f5abcce3d892f96fa04e8199a2d6cf08
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2025-05-26 18:12:00 +0900

    Add spacer in column

--- app/src/main/java/com/hjw/app/material3preview/MainActivity.kt
+++ app/src/main/java/com/hjw/app/material3preview/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,8 +36,11 @@ class MainActivity : ComponentActivity() {
                     ) {
                         Title(name = "Loading Indicators")
                         LoadingIndicators()
+                        Spacer(modifier = Modifier.size(12.dp))
+
                         Title(name = "Filled Split Button")
                         FilledSplitButton()
+                        Spacer(modifier = Modifier.size(12.dp))
                     }
                 }
             }
